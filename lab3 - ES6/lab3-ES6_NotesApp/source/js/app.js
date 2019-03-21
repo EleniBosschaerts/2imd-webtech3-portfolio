b619f2393ee60d5494d8b16c7c2ce2ebc0fe06fc
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-03-21 20:36:27 +0100

    Update app.js
    HET WERKT EINDELIJK!
    met knop en enter

--- lab3 - ES6/lab3-ES6_NotesApp/source/js/app.js
+++ lab3 - ES6/lab3-ES6_NotesApp/source/js/app.js
@@ -1,13 +1,15 @@
+let i = 0;
 class Note {
   constructor(title) {
     this.title = title;
     this.element = this.createElement(title);
   }
-  createElement(title){
+  
+    createElement(title){
     // binnen een element > newNote (geen 2 keer var note mogelijk)
     // class - div met alles in 
     let newNote = document.createElement('div');
-    newNote.innerHTML = `<p>${this.title}</p><br><a href="#" class="card-remove">Remove</a>`;
+    newNote.innerHTML = `<p>${this.title}</p> <a href="#" class="card-remove">Remove</a>`;
     newNote.setAttribute("class", "card");
 
    // promise toegevoegd - remove klik
@@ -22,99 +24,90 @@ class Note {
     return newNote;
   }
   
-  add(){ //note.element
+  add(element){  //note.element
     // HINT🤩
     // this function should append the note to the screen somehow
-    let notes = document.querySelector('.notes');
-    notes.appendChild(this.element);
+    document.querySelector(".notes").appendChild(element);
   }
   
-  saveToStorage(){
+  saveToStorage(noteText){
     // HINT🤩
     // localStorage only supports strings, not arrays
     // if you want to store arrays, look at JSON.parse and JSON.stringify
     
-    let data = JSON.parse(localStorage.getItem('keyNoteTitel'));
-    if (data == null) {
-      data = [];
-    }
-    data.push(this.title);
-    localStorage.setItem('keyNoteTitel', JSON.stringify(data));
-    // localStorage.setItem(tabelKolom, data er in); 
-    
-    
-    /*let arr = [];
-    if(localStorage.length > 0){
-        arr = JSON.parse(localStorage.getItem("nodes"));
-    }
-    arr.push(title);
+    let arrStorage = [];
 
-    localStorage.setItem("nodes", JSON.stringify(arr));
-    */
+    if (localStorage.length > 0) {
+      arrStorage = JSON.parse(localStorage.getItem("nodes"));
+
+      const arrCount = arrStorage.length;
+      arrStorage[arrCount] = noteText;
+    } else {
+      arrStorage[0] = noteText;
+    }
+
+    localStorage.setItem("nodes", JSON.stringify(arrStorage));
   }
   
   remove(){
     // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
-    // in this function, 'this' will refer to the current note element
-    setTimeout( ()=>{
-      this.parentNode.removeChild(this);
-      this.parentNode.style.display = "none";
+    // in this function, 'this' will refer to the current note createElement
+    
+    setTimeout(() => {
+      this.style.display = "none";
     }, 1000);
-
-    let arr = [];
-    arr = JSON.parse(localStorage.getItem("nodes"));
-    arr.splice(arr.indexOf(this), 1);
-    localStorage.setItem("nodes", JSON.stringify(arr));
-  } 
+  }
 }
 
 class App {
   constructor() {
     console.log("👊🏼 The Constructor!");
-    
+   
     // HINT🤩
     // clicking the button should work
     this.btnAdd = document.querySelector("#btnAddNote");
     this.btnAdd.addEventListener("click", this.createNote.bind(this));
+    
     // pressing the enter key should also work
-    this.txtAdd = document.querySelector("#txtAddNote");
-    this.txtAdd.addEventListener("keydown", event => {
+    this.input = document.querySelector("#txtAddNote");
+    this.input.addEventListener("keydown", event => {
       if(event.keyCode === 13){
         this.createNote();
       }
     });
+
     this.loadNotesFromStorage();
   }
+  
   
   loadNotesFromStorage() {
     // HINT🤩
     // load all notes from storage here and add them to the screen
     // something like note.add() in a loop would be nice
-    if(localStorage.length > 0){
-      let arr = [];
-      arr = JSON.parse(localStorage.getItem("nodes"));
-      for(let i = 0; i < arr.length; i++){
-        let note = new Note(arr[i]);
+    const loadNotes = JSON.parse(localStorage.getItem("nodes"));
+    
+    if (loadNotes.length > 0) {
+      loadNotes.forEach(notes => {
+        let note = new Note(notes);
         note.add(note.element);
-      }
+      });
     }
   }
-  
-  createNote(e){  
+
+  createNote(){
+    const textInNote = document.querySelector("#txtAddNote").value;
     // this function should create a new note by using the Note() class
-    let textInNote = document.querySelector("#txtAddNote").value;
     let note = new Note(textInNote);
-    
     // HINT🤩
-    note.add(); //(note.element)
+    note.add(note.element);
     note.saveToStorage();
     this.reset();
   }
   
   reset(){
-    // this function should reset the form 
-    document.querySelector("txtAddNote").value = "";
+    // this function should reset the form
+    document.querySelector("#txtAddNote").value = "";
   }
+  
 }
-
 let app = new App();
