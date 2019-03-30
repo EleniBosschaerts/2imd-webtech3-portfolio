5b3167b824af82813529e054bd653277697dd147
EleniBosschaerts <eleni.bosschaerts@gmail.com>
2019-03-31 00:30:22 +0100

    basic routing added + map structure

--- lab5 - NodeJS/NodeJSBasic/app.js
+++ lab5 - NodeJS/NodeJSBasic/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/api/v1/index');
+var usersRouter = require('./routes/api/v1/users');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/messagesdemo', {useNewUrlParser: true});
 
